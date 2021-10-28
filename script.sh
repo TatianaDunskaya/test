f098625123dacc590a1472802a8c363d11ca43f3
Tetiana Duncka <tetianaduncka@MacBook-Pro-Tetiana.local>
2021-10-28 12:02:01 +0300

    test

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in {1..10}; do sleep 2 && ps u -p 94992 | awk '{print $2, $3, $4, $6}'; done >> test.txt
+for i in {1..10}; do sleep 2 && ps u -p 94992 | awk '{print $2, $3, $4, $6,$7, $8}'; done >> test.txt
