7e26b260291f92b1bfed005daf770fe1c1c2fa77
Tetiana Duncka <tetianaduncka@MacBook-Pro-Tetiana.local>
2021-10-29 12:55:57 +0300

    fix conflict

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i test2 test3  in {1..10}; do sleep 2 && ps u -p 94992 | awk '{print $2, $3, $4, $6,$7, $8}'; done >> test.txt
+for i test2 in {1..10}; do sleep 2 && ps u -p 94992 | awk '{print $2, $3, $4, $6,$7, $8}'; done >> test.txt
